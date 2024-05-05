377ad6c0e0e4d2184a00800e95119c17cf78631d
Dave Pusey <dave@puseyuk.co.uk>
2024-05-05 11:21:38 +0100

    Update to 5.1.3.1

---
+++ src/libFetchrActiveItems/ItemPool.cs
@@ -0,0 +1,35 @@
+﻿using libFetchrActiveItems.DataStructures;
+using libFetchrVersion;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace libFetchrActiveItems
+{
+	public class ItemPool
+	{
+		private static readonly ItemPoolSorter itemPoolSorter = new();
+
+		public static Dictionary<string, List<ItemData>> GetItemPool(FetchrVersion fetchrVersion)
+		{
+			List<ItemData> activeItems = ActiveItems.Get(fetchrVersion);
+
+			Dictionary<string, List<ItemData>> itemPool = [];
+
+			foreach (ItemData item in activeItems)
+			{
+				foreach (CategoryData category in item.ActiveCategories)
+				{
+					if (itemPool.ContainsKey(category.Id) == false) itemPool.Add(category.Id, []);
+					itemPool[category.Id].Add(item);
+				}
+			}
+
+			return itemPool;
+		}
+
+		public static Dictionary<string, List<ItemData>> GetSortedItemPool(FetchrVersion fetchrVersion)
+		{
+			return GetItemPool(fetchrVersion).Order(itemPoolSorter).ToDictionary();
+		}
+	}
+}
---
+++ src/libFetchrActiveItems/ItemPoolSorter.cs
@@ -0,0 +1,14 @@
+﻿using libFetchrActiveItems.DataStructures;
+using System.Collections.Generic;
+
+namespace libFetchrActiveItems
+{
+	internal class ItemPoolSorter : IComparer<KeyValuePair<string, List<ItemData>>>
+	{
+		public int Compare(KeyValuePair<string, List<ItemData>> x, KeyValuePair<string, List<ItemData>> y)
+		{
+			if (x.Value.Count == y.Value.Count) return x.Key.CompareTo(y.Key);
+			return y.Value.Count.CompareTo(x.Value.Count);
+		}
+	}
+}
